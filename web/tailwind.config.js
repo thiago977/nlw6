17bfc8cebde2779dc93b7e06b005840d5a7bc84f
Thiago de Lima <thiagodelima1997@gmail.com>
2023-05-18 19:32:00 -0300

    web layout

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,10 +7,62 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: 'var(--font-roboto)',
+        alt: 'var(--font-bai-jamjuree)',
+      },
+      colors: {
+        gray: {
+          50: '#eaeaea',
+          100: '#bebebf',
+          200: '#9e9ea0',
+          300: '#727275',
+          400: '#56565a',
+          500: '#2c2c31',
+          600: '#28282d',
+          700: '#1f1f23',
+          800: '#18181b',
+          900: '#121215',
+        },
+        purple: {
+          50: '#f3eefc',
+          100: '#d8cbf7',
+          200: '#c6b2f3',
+          300: '#ab8eee',
+          400: '#9b79ea',
+          500: '#8257e5',
+          600: '#764fd0',
+          700: '#5c3ea3',
+          800: '#48307e',
+          900: '#372560',
+        },
+        orange: {
+          50: '#ffefeb',
+          100: '#ffccc2',
+          200: '#ffb4a4',
+          300: '#ff927b',
+          400: '#ff7d61',
+          500: '#ff5c3a',
+          600: '#e85435',
+          700: '#b54129',
+          800: '#8c3320',
+          900: '#6b2718',
+        },
+      },
+
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        stripes:
+          'linear-gradient(to bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1) 12.5%, transparent 12.5%, transparent)',
+      },
+
+      backgroundSize: {
+        stripes: '100% 8px',
+      },
+      blur: {
+        full: '194px',
+      },
+      fontSize: {
+        '5xl': '2.5rem',
       },
     },
   },
